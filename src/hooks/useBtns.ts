c6b4c067d9fe61cdd0b91c60b016f3025958568a
liulihao <551772063@qq.com>
2024-11-29 10:43:33 +0800

    feat: 更新table

--- src/hooks/useBtns.ts
+++ src/hooks/useBtns.ts
@@ -1,4 +1,4 @@
-import { ref, nextTick, getCurrentInstance } from 'vue'
+import { ref, nextTick, getCurrentInstance, computed } from 'vue'
 import { preview } from '@/utils/remoteFunc.ts'
 import { isImage, gDownload } from '@/utils/gFunc.ts'
 import { $toast } from 'oeos-components'
@@ -7,7 +7,7 @@ import { deleteOne, objectPropertyDetail } from '@/api/bucketReview'
 
 export const useBtns = (RestoreExpirationInDaysRef, bucketOverviewHistoryRef, BucketFileDetailsCompRef, init) => {
   const { proxy } = getCurrentInstance()
-  const btns = ref([])
+  // const btns = ref([])
   const isChange = ref(false)
   const detailRow = async (row) => {
     let params = {
@@ -27,82 +27,74 @@ export const useBtns = (RestoreExpirationInDaysRef, bucketOverviewHistoryRef, Bu
     $toast('删除成功')
     init()
   }
-  btns.value = {
-    label: '操作',
-    prop: 'operation',
-    width: proxy.$dev ? 300 : 200,
-    maxBtns: proxy.$dev ? 10 : 3,
-    isShow: (row) => {
-      return row.injectTime ? true : false
-    },
-    btns: [
-      {
-        content: '详情',
-        handler: detailRow,
-        comp: 'o-icon',
-        attrs: {
-          type: 'svg',
-          name: 'detail',
+  const btns = computed(() => {
+    let baseObj = {
+      label: '操作',
+      prop: 'operation',
+
+      isShow: (row) => {
+        return row.injectTime ? true : false
+      },
+      btns: [
+        {
           content: '详情',
-          size: 5,
+          handler: detailRow,
+          comp: 'o-icon',
+          attrs: {
+            type: 'svg',
+            name: 'detail',
+            content: '详情',
+            size: 5,
+          },
         },
-      },
-      {
-        content: '下载',
-        handler: gDownload,
-        comp: 'o-icon',
-        attrs: {
-          type: 'svg',
-          name: 'download',
+        {
           content: '下载',
-          size: 6,
+          handler: gDownload,
+          comp: 'o-icon',
+          attrs: {
+            type: 'svg',
+            name: 'download',
+            content: '下载',
+            size: 6,
+          },
         },
-      },
-      {
-        content: '删除',
-        handler: deleteRow,
-        comp: 'o-icon',
-        attrs: {
-          name: 'delete',
-          type: 'svg',
-          size: 6,
+        {
           content: '删除',
+          handler: deleteRow,
+          comp: 'o-icon',
+          attrs: {
+            name: 'delete',
+            type: 'svg',
+            size: 6,
+            content: '删除',
+          },
+          reConfirm: !proxy.$dev,
         },
-        reConfirm: !proxy.$dev,
-      },
-      {
-        content: '恢复',
-        // comp: 'o-icon',
-        // attrs: {
-        //   type: 'svg',
-        //   name: 'restore',
-        //   content: '恢复',
-        //   size: 6,
-        // },
-        handler: (row) => {
-          RestoreExpirationInDaysRef.value.open(row)
+        {
+          content: '恢复',
+          handler: (row) => {
+            RestoreExpirationInDaysRef.value.open(row)
+          },
         },
-      },
-      {
-        content: '历史',
-        handler: (row) => bucketOverviewHistoryRef.value.open(row),
-        // comp: 'o-icon',
-        // attrs: {
-        //   type: 'svg',
-        //   name: 'history',
-        //   content: '历史',
-        //   size: 6,
-        // },
-      },
-      {
-        content: '预览',
-        isShow: (row) => {
-          return isImage(row.key)
+        {
+          content: '历史',
+          handler: (row) => bucketOverviewHistoryRef.value.open(row),
         },
-        handler: (row) => preview(row.bucket, row.name),
-      },
-    ],
-  }
+        {
+          content: '预览',
+          isShow: (row) => {
+            return isImage(row.key)
+          },
+          handler: (row) => preview(row.bucket, row.name),
+        },
+      ],
+    }
+    if (proxy.$dev) {
+      baseObj.width = 300
+      baseObj.maxBtns = 10
+    }
+    return baseObj
+  })
   return {
     btns,
     isChange,
