9a9bac863721162671be9a860838efdc0e0e1922
liulihao <551772063@qq.com>
2024-12-02 15:16:49 +0800

    feat: 更新脚本

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ fi
 for i in {33..35};do
 # for i in 33;do
    ssh  root@10.0.11."$i" <<EOF
-   rm -rf /opt/newfront/tenant/tenant
+   rm -rf /usr/local/oct/oeos/app/KS02/front/tenant/tenant
 EOF
-   scp -r ./tenant/* root@10.0.11."$i":/opt/newfront/tenant/tenant
+   scp -r ./tenant/* root@10.0.11."$i":/usr/local/oct/oeos/app/KS02/front/tenant/tenant
    ssh root@10.0.11."$i" systemctl restart nginx
 done
